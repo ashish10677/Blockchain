b71cbe86cb1bf8509736bda5be795dd267c970ca
ashish10677 <ashish10677@gmail.com>
2020-02-22 15:59:54 +0530

    Refactor code.
    Signed-off-by: ashish10677 <ashish10677@gmail.com>

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,6 @@
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
 
 
 class Blockchain:
@@ -51,49 +50,3 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
-
-
-# Creating a web app
-app = Flask(__name__)
-
-blockchain = Blockchain()
-
-
-# Mine a new block
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    previous_block = blockchain.get_previous_block()
-    previous_proof = previous_block['proof']
-    proof = blockchain.proof_of_work(previous_proof)
-    previous_hash = blockchain.hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
-    response = {
-        "message": "Congratulations, you just mined a block",
-        "index": block["index"],
-        "timestamp": block["timestamp"],
-        "nonce": block["proof"],
-        "previous_hash": block["previous_hash"]
-    }
-    return jsonify(response), 200
-
-
-# Get the entire blockchain
-@app.route('/get_chain', methods=['GET'])
-def get_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain)
-    }
-    return jsonify(response), 200
-
-
-@app.route('/is_valid', methods=['GET'])
-def is_valid():
-    validity = blockchain.is_chain_valid(blockchain.chain)
-    response = {
-        'is_valid': validity
-    }
-    return jsonify(response), 200
-
-
-app.run(host='0.0.0.0', port=5000)
